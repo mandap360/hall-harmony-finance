ac6fc22508cf5989f71eb2f7ef1de28562912861
Code Queen <mandap360@123>
2025-09-29 12:09:07 +0530

    PWA Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Mandap360.app',
+  appName: 'Mandap360',
+  webDir: 'dist'
+};
+
+export default config;
